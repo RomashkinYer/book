a66aca67c42bc4697e4d31f0082955d779f236f4
Romashkin <roma_erilov@mail.ru>
2024-12-17 03:12:27 +1000

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    private String title;
+    private int releaseYear;
+    private String author;
+    private int pages;
+
+    //Конструктор
+    public Book(String title, int releaseYear, String author, int pages){
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
